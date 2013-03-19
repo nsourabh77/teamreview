13a2f09087259032bce16c3ff54f961da8d154dc
Oliver <oliver.friedrich@gmail.com>
2013-03-19 21:16:20 +0100

    initial solution setup with projects, packages, and start page

---
+++ TeamReview.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TeamReview.Controllers {
+	public class HomeController : Controller {
+		//
+		// GET: /Home/
+
+		public ActionResult Index() {
+			return View();
+		}
+	}
+}
