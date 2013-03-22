0a8fa17fbe83ae4f2e48dd71d1ebe12662204488
Oliver <oliver.friedrich@gmail.com>
2013-03-22 23:37:37 +0100

    add test using Massive with SQL CE

---
+++ TeamReview.Tests/MassiveSqlCeTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using Massive;
+using NUnit.Framework;
+
+namespace TeamReview.Tests {
+	[TestFixture]
+	public class MassiveSqlCeTest {
+		[Test]
+		public void Should_connect_to_sql_ce_db() {
+			var dbPath = Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.FullName,
+			                          "TeamReview.Web", "App_Data", "TeamReview.sdf");
+			var connectionString = string.Format("Data Source={0};Persist Security Info=False;", dbPath);
+			var database = new DynamicModel(true, connectionString, "UserProfile", "UserId");
+			foreach (var result in database.Query("SELECT UserName FROM UserProfile")) {
+				Console.WriteLine(result);
+			}
+		}
+	}
+}
