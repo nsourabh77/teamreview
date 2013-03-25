7a3735f6fbca530c7184fe766e88bb5964081359
Anton <anton@teamaton.com>
2013-03-25 18:48:03 +0100

    Specs: CreateReview erweitern, GiveReview anfangen

--- TeamReview.Specs/CreateReview.feature
+++ TeamReview.Specs/CreateReview.feature
@@ -14,12 +14,34 @@ Scenario: Create a review with 2 categories
 		And I fill in a category name
 		And I fill in a category description
 		And I save the review
-	Then my new review was created
+	Then my new review was created with those categories
+		And I am added to the review
 		And I am on the "Edit review" page for my review
+		And I see the message "Review has been saved"
+
+Scenario: Add category to existing Review
+	Given I am logged in
+		And I own a review
+	When I edit my review
+		And I add another category
+		And I fill in a category name
+		And I fill in a category description
+		And I save the review
+	Then my review is updated with the new category
+		And I am on the "Edit review" page for my review
+		And I see the message "Review has been saved"
+
+Scenario: Show edit link to review on dashboard
+	Given I am logged in
+		And I own a review
+		And I am on the "Dashboard"
+	Then I see a link to the "Edit review" page of the review
+	When I click on the "Edit review" link
+	Then I am on the "Edit review" page for my review
 
 Scenario: Invite new peer to my review
 	Given I am logged in
-		And I have a review
+		And I own a review
 	When I edit my review
 		And I invite a peer
 		And I fill in the peer's name
@@ -27,15 +49,15 @@ Scenario: Invite new peer to my review
 		And I invite that peer
 		And no account exists for that peer's email address
 	Then a new user with the given name and email address was created
-		And an email was sent to that user containing a link to the "Give a review" page of my review
+		And this user is added to the review
 
 Scenario: Invite existing peer to my review
 	Given I am logged in
-		And I have a review
+		And I own a review
 	When I edit my review
 		And I invite a peer
 		And I fill in the peer's name
 		And I fill in the peer's email address
 		And I invite that peer
 		And an account exists for that peer's email address
-	Then an email was sent to that user containing a link to the "Give a review" page of my review
+	Then this user is added to the review
--- TeamReview.Specs/CreateReview.feature.cs
+++ TeamReview.Specs/CreateReview.feature.cs
@@ -92,9 +92,67 @@ this.ScenarioSetup(scenarioInfo);
 #line 16
   testRunner.And("I save the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 17
- testRunner.Then("my new review was created", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+ testRunner.Then("my new review was created with those categories", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line 18
+  testRunner.And("I am added to the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 19
   testRunner.And("I am on the \"Edit review\" page for my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 20
+  testRunner.And("I see the message \"Review has been saved\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Add category to existing Review")]
+        public virtual void AddCategoryToExistingReview()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Add category to existing Review", ((string[])(null)));
+#line 22
+this.ScenarioSetup(scenarioInfo);
+#line 23
+ testRunner.Given("I am logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 24
+  testRunner.And("I own a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 25
+ testRunner.When("I edit my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 26
+  testRunner.And("I add another category", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 27
+  testRunner.And("I fill in a category name", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 28
+  testRunner.And("I fill in a category description", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 29
+  testRunner.And("I save the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 30
+ testRunner.Then("my review is updated with the new category", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 31
+  testRunner.And("I am on the \"Edit review\" page for my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 32
+  testRunner.And("I see the message \"Review has been saved\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Show edit link to review on dashboard")]
+        public virtual void ShowEditLinkToReviewOnDashboard()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Show edit link to review on dashboard", ((string[])(null)));
+#line 34
+this.ScenarioSetup(scenarioInfo);
+#line 35
+ testRunner.Given("I am logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 36
+  testRunner.And("I own a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 37
+  testRunner.And("I am on the \"Dashboard\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 38
+ testRunner.Then("I see a link to the \"Edit review\" page of the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 39
+ testRunner.When("I click on the \"Edit review\" link", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 40
+ testRunner.Then("I am on the \"Edit review\" page for my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
@@ -104,29 +162,28 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void InviteNewPeerToMyReview()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Invite new peer to my review", ((string[])(null)));
-#line 20
+#line 42
 this.ScenarioSetup(scenarioInfo);
-#line 21
+#line 43
  testRunner.Given("I am logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line 22
-  testRunner.And("I have a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 23
+#line 44
+  testRunner.And("I own a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 45
  testRunner.When("I edit my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 24
+#line 46
   testRunner.And("I invite a peer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 25
+#line 47
   testRunner.And("I fill in the peer\'s name", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 26
+#line 48
   testRunner.And("I fill in the peer\'s email address", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 27
+#line 49
   testRunner.And("I invite that peer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 28
+#line 50
   testRunner.And("no account exists for that peer\'s email address", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 29
+#line 51
  testRunner.Then("a new user with the given name and email address was created", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line 30
-  testRunner.And("an email was sent to that user containing a link to the \"Give a review\" page of m" +
-                    "y review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 52
+  testRunner.And("this user is added to the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             this.ScenarioCleanup();
         }
@@ -136,27 +193,26 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void InviteExistingPeerToMyReview()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Invite existing peer to my review", ((string[])(null)));
-#line 32
+#line 54
 this.ScenarioSetup(scenarioInfo);
-#line 33
+#line 55
  testRunner.Given("I am logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line 34
-  testRunner.And("I have a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 35
+#line 56
+  testRunner.And("I own a review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 57
  testRunner.When("I edit my review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 36
+#line 58
   testRunner.And("I invite a peer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 37
+#line 59
   testRunner.And("I fill in the peer\'s name", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 38
+#line 60
   testRunner.And("I fill in the peer\'s email address", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 39
+#line 61
   testRunner.And("I invite that peer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 40
+#line 62
   testRunner.And("an account exists for that peer\'s email address", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line 41
- testRunner.Then("an email was sent to that user containing a link to the \"Give a review\" page of m" +
-                    "y review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 63
+ testRunner.Then("this user is added to the review", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
