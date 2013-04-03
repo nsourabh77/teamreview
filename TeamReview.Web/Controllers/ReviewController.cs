9f38dd4c127e3f23031251445d0b3cb4bb212867
Oliver <oliver.friedrich@gmail.com>
2013-04-03 15:56:05 +0200

    Change AddCategory to not use javascript at all! just use simple MVC actions; edit view needs to be adapted

--- TeamReview.Web/Controllers/ReviewController.cs
+++ TeamReview.Web/Controllers/ReviewController.cs
@@ -1,35 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using TeamReview.Web.Models;
 
-namespace TeamReview.Web.Controllers
-{
+namespace TeamReview.Web.Controllers {
 	[Authorize]
-	public class ReviewController : Controller
-	{
-		private ReviewsContext db = new ReviewsContext();
+	public class ReviewController : Controller {
+		private readonly ReviewsContext db = new ReviewsContext();
 
 		//
 		// GET: /Review/
 
-		public ActionResult Index()
-		{
+		public ActionResult Index() {
 			return View(db.ReviewConfigurations.ToList());
 		}
 
 		//
 		// GET: /Review/Details/5
 
-		public ActionResult Details(int id = 0)
-		{
-			ReviewConfiguration reviewconfiguration = db.ReviewConfigurations.Find(id);
-			if (reviewconfiguration == null)
-			{
+		public ActionResult Details(int id = 0) {
+			var reviewconfiguration = db.ReviewConfigurations.Find(id);
+			if (reviewconfiguration == null) {
 				return HttpNotFound();
 			}
 			return View(reviewconfiguration);
@@ -37,44 +27,50 @@ namespace TeamReview.Web.Controllers
 
 		//
 		// GET: /Review/Create
-		public ActionResult Create()
-		{
-			return View(new ReviewConfiguration());
+
+		public ActionResult Create() {
+			object review;
+			return View(TempData.TryGetValue("review", out review) ? review : new ReviewConfiguration());
 		}
 
 		//
 		// POST: /Review/Create
 
 		[HttpPost]
-		public ActionResult Create(ReviewConfiguration reviewconfiguration)
-		{
-			if (ModelState.IsValid)
-			{
-				reviewconfiguration.Peers.Add(db.UserProfiles.First(user => user.UserName == User.Identity.Name));
-				db.ReviewConfigurations.Add(reviewconfiguration);
+		public ActionResult Create(ReviewConfiguration reviewConfiguration) {
+			if (Request.Form["reviewAction"] == "AddCategory") {
+				reviewConfiguration.Categories.Add(new ReviewCategory());
+			}
+			else if (Request.Form["reviewAction"] == "AddPeer") {
+				reviewConfiguration.Peers.Add(new UserProfile());
+			}
+			else if (ModelState.IsValid) {
+				// TODO: check for duplicates, maybe user added himself or someone else twice
+				reviewConfiguration.Peers.Add(db.UserProfiles.First(user => user.UserName == User.Identity.Name));
+				db.ReviewConfigurations.Add(reviewConfiguration);
 				db.SaveChanges();
 				TempData["Message"] = "Review has been created";
-				return RedirectToAction("Edit", new {id = reviewconfiguration.ReviewId});
+				return RedirectToAction("Edit", new { id = reviewConfiguration.ReviewId });
 			}
 
-			return View(reviewconfiguration);
+			TempData["review"] = reviewConfiguration;
+			return RedirectToAction("Create");
+			//return View(reviewConfiguration);
 		}
-		
-		public ActionResult AddCategory(ReviewConfiguration reviewConfiguration)
-		{
-			reviewConfiguration.Categories.Add(new ReviewCategory());
-			return View("Create", reviewConfiguration);
-			return PartialView("AddCategory", new ReviewCategory());
-		}
+
+		//public ActionResult AddCategory(ReviewConfiguration reviewConfiguration) {
+		//    reviewConfiguration.Categories.Add(new ReviewCategory());
+		//    TempData["review"] = reviewConfiguration;
+		//    return RedirectToAction("Create");
+		//    //return PartialView("AddCategory", new ReviewCategory());
+		//}
 
 		//
 		// GET: /Review/Edit/5
 
-		public ActionResult Edit(int id = 0)
-		{
+		public ActionResult Edit(int id = 0) {
 			var reviewconfiguration = db.ReviewConfigurations.Find(id);
-			if (reviewconfiguration == null)
-			{
+			if (reviewconfiguration == null) {
 				return HttpNotFound();
 			}
 			db.Entry(reviewconfiguration).Collection(c => c.Categories).Load();
@@ -85,10 +81,8 @@ namespace TeamReview.Web.Controllers
 		// POST: /Review/Edit/5
 
 		[HttpPost]
-		public ActionResult Edit(ReviewConfiguration reviewconfiguration)
-		{
-			if (ModelState.IsValid)
-			{
+		public ActionResult Edit(ReviewConfiguration reviewconfiguration) {
+			if (ModelState.IsValid) {
 				var dbReviewConfiguration = db.ReviewConfigurations.Find(reviewconfiguration.ReviewId);
 				db.Entry(dbReviewConfiguration).Collection(c => c.Categories).Load();
 				UpdateModel(dbReviewConfiguration);
@@ -102,11 +96,9 @@ namespace TeamReview.Web.Controllers
 		//
 		// GET: /Review/Delete/5
 
-		public ActionResult Delete(int id = 0)
-		{
-			ReviewConfiguration reviewconfiguration = db.ReviewConfigurations.Find(id);
-			if (reviewconfiguration == null)
-			{
+		public ActionResult Delete(int id = 0) {
+			var reviewconfiguration = db.ReviewConfigurations.Find(id);
+			if (reviewconfiguration == null) {
 				return HttpNotFound();
 			}
 			return View(reviewconfiguration);
@@ -116,16 +108,14 @@ namespace TeamReview.Web.Controllers
 		// POST: /Review/Delete/5
 
 		[HttpPost, ActionName("Delete")]
-		public ActionResult DeleteConfirmed(int id)
-		{
-			ReviewConfiguration reviewconfiguration = db.ReviewConfigurations.Find(id);
+		public ActionResult DeleteConfirmed(int id) {
+			var reviewconfiguration = db.ReviewConfigurations.Find(id);
 			db.ReviewConfigurations.Remove(reviewconfiguration);
 			db.SaveChanges();
 			return RedirectToAction("Index");
 		}
 
-		protected override void Dispose(bool disposing)
-		{
+		protected override void Dispose(bool disposing) {
 			db.Dispose();
 			base.Dispose(disposing);
 		}
