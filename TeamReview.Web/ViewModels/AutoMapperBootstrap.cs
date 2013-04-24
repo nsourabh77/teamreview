0aefd44117f362cf49251fae258313be58a2adb5
Oliver <oliver.friedrich@gmail.com>
2013-04-24 22:40:55 +0200

    reuse Create review view for Editing reviews, make Add Category/Add Peer links work replacing them with buttons

--- TeamReview.Web/ViewModels/AutoMapperBootstrap.cs
+++ TeamReview.Web/ViewModels/AutoMapperBootstrap.cs
@@ -4,10 +4,10 @@ using TeamReview.Web.Models;
 namespace TeamReview.Web.ViewModels {
 	public class AutoMapperBootstrap {
 		public static void Initialize() {
-			Mapper.CreateMap<ReviewConfiguration, ReviewCreateModel>().ReverseMap();
-			Mapper.CreateMap<ReviewConfiguration, ReviewEditModel>()
+			Mapper.CreateMap<ReviewConfiguration, ReviewCreateEditModel>()
 				.ForMember(model => model.ExistingCategories, opt => opt.ResolveUsing(review => review.Categories))
-				.ForMember(model => model.ExistingPeers, opt => opt.ResolveUsing(review => review.Peers));
+				.ForMember(model => model.ExistingPeers, opt => opt.ResolveUsing(review => review.Peers))
+				.ReverseMap();
 			Mapper.CreateMap<ReviewCategory, CategoryAddModel>().ReverseMap();
 			Mapper.CreateMap<ReviewCategory, CategoryShowModel>();
 			Mapper.CreateMap<UserProfile, PeerAddModel>().ReverseMap();
