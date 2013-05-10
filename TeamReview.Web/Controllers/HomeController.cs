162d86276a08508cd71203dfba562710881ca375
Oliver <oliver.friedrich@gmail.com>
2013-05-10 15:04:03 +0200

    Send email from Contact form

--- TeamReview.Web/Controllers/HomeController.cs
+++ TeamReview.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Mail;
+using System.Web.Mvc;
+using TeamReview.Web.ViewModels;
 
 namespace TeamReview.Controllers {
 	public class HomeController : Controller {
@@ -9,40 +11,52 @@ namespace TeamReview.Controllers {
 			return View();
 		}
 
-		public ActionResult Features()
-		{
+		public ActionResult Features() {
 			return View();
 		}
 
-		public ActionResult StackRanking()
-		{
+		public ActionResult StackRanking() {
 			return View();
 		}
 
-		public ActionResult PeerReviews()
-		{
+		public ActionResult PeerReviews() {
 			return View();
 		}
 
-		public ActionResult PerformanceReview()
-		{
+		public ActionResult PerformanceReview() {
 			return View();
 		}
 
-		public ActionResult VisualizeData()
-		{
+		public ActionResult VisualizeData() {
 			return View();
 		}
 
-		public ActionResult AboutUs()
-		{
+		public ActionResult AboutUs() {
 			return View();
 		}
 
-		public ActionResult Contact()
-		{
+		public ActionResult Contact() {
 			return View();
 		}
 
+		[HttpPost]
+		public ActionResult Contact(ContactViewModel userdata) {
+			if (!ModelState.IsValid) {
+				return View();
+			}
+
+			var message = new MailMessage("teamreview@teamaton.com", "hello@teamreview.net")
+			              	{
+			              		Body = userdata.Message,
+			              		Subject = "TeamReview.net - contact form"
+			              	};
+			var displayName = userdata.GetDisplayName();
+			message.ReplyToList.Add(new MailAddress(userdata.EmailAddress, displayName));
+
+			new SmtpClient().Send(message);
+
+			TempData["MessageSent"] = "true";
+			return RedirectToAction("Contact");
+		}
 	}
 }
